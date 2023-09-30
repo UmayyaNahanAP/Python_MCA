f3dd0b68fe38f58bf28f5b6f8fb50ec020be0681
Umayya Nahan . A P <88381959+UmayyaNahanAP@users.noreply.github.com>
2023-09-30 16:17:40 +0530

    Add files via upload

---
+++ leapyr2.py
@@ -0,0 +1,6 @@
+cy=int(input("Enter the current year : "))
+fy=int(input("Enter the final year : "))
+print("Leap year from",cy,"to",fy,"are : ")
+for y in range(cy,fy+1):
+    if(y%4==0 and y%100!=0 or y%400==0):
+         print(y,end=" ")
