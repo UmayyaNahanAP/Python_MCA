c059fc1f7dcdf3414e617daf9ad603b2b362e0ba
Umayya Nahan . A P <88381959+UmayyaNahanAP@users.noreply.github.com>
2023-10-02 06:43:59 +0530

    Update leapyr2.py

--- leapyr2.py
+++ leapyr2.py
@@ -1,3 +1,4 @@
+#Display future leap years from current year to a final year entered by the user
 cy=int(input("Enter the current year : "))
 fy=int(input("Enter the final year : "))
 print("Leap year from",cy,"to",fy,"are : ")
