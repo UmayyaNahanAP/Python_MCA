9c0be259b7eb9b884d25874fe3aeb02a01197841
Umayya Nahan . A P <88381959+UmayyaNahanAP@users.noreply.github.com>
2023-10-13 15:42:41 +0530

    Add files via upload

---
+++ dictionary/dict7.py
@@ -0,0 +1,9 @@
+d={'Admi_no':18284,'name':'Umayya','age':21,'Department':'Computer Application'}
+print("Dictionary  :")
+print(d)
+print("Dictionary after pop :")
+d.pop('age')
+print(d)
+print("Dictionary after popitem :")
+d.popitem()
+print(d)
