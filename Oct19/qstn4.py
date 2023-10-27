667a4d24de799a20e58243bd064322e6ff07063b
Umayya Nahan . A P <88381959+UmayyaNahanAP@users.noreply.github.com>
2023-10-27 16:02:19 +0530

    Add files via upload

---
+++ Oct19/qstn4.py
@@ -0,0 +1,4 @@
+s1 = input("Enter a string: ")
+s2 = input("Enter another string: ")
+s= s2[0]+s1[1:]+" "+ s1[0] + s2[1:]
+print(f"New string : {s}")
---
+++ Oct19/qstn5.py
@@ -0,0 +1,4 @@
+d={'c':1,'b':4,'d':6,'a':10,'f':7,'e':2}
+print(f"Orginal dictionary : {d}")
+print("Sorted in acending order : ", sorted(d.items()))
+print("Sorted descending order : ", sorted(d.items(),reverse=True))
