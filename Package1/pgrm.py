55f0b663a19720a238ddaf7999e20a2481ca7a3c
Umayya Nahan . A P <88381959+UmayyaNahanAP@users.noreply.github.com>
2023-11-16 15:04:15 +0530

    Add files via upload

---
+++ Package1/pgrm.py
@@ -0,0 +1,7 @@
+from package import sum
+from package import avg
+n1=int(input("Enter the first number : "))
+n2=int(input("Enter the secound number : "))
+
+print("Sum : ",sum.sum(n1,n2))
+print("Average : ",avg.avg(n1,n2))
