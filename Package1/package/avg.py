e020f6ddea9001c58a5377a90fb5e18e545b4d25
Umayya Nahan . A P <88381959+UmayyaNahanAP@users.noreply.github.com>
2023-11-16 15:26:29 +0530

    Add files via upload

---
+++ Package1/package/avg.py
@@ -0,0 +1,4 @@
+def avg(n1,n2):
+    a=(n1+n2)/2
+    return(a)
+    
--- Package1/pgrm.py
+++ Package1/pgrm.py
@@ -1,7 +1,10 @@
 from package import sum
 from package import avg
+from package import power
 n1=int(input("Enter the first number : "))
 n2=int(input("Enter the secound number : "))
 
 print("Sum : ",sum.sum(n1,n2))
 print("Average : ",avg.avg(n1,n2))
+print(f"Power of {n1}: {power.power(n1)}")
+print(f"Power of {n2}: {power.power(n2)}")
