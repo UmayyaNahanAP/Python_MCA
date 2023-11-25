2170c30d90b681a1e4108d02245dff20a18247e3
Umayya Nahan . A P <88381959+UmayyaNahanAP@users.noreply.github.com>
2023-11-25 16:00:38 +0530

    Add files via upload

---
+++ Package3/pgrm.py
@@ -0,0 +1,8 @@
+from graphics import rectangle
+from graphics import circle
+from graphics._3d_graphics import sphere
+from graphics._3d_graphics import cuboid
+rectangle_area=rectangle.rectangle_area(3,4)
+circle_area=circle.circle_area(3)
+sphere_area=sphere.sphere_area(5)
+cuboid_area=cuboid.cuboid_area(2,1,3)
